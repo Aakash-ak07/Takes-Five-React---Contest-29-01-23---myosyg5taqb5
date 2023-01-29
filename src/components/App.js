336676ff49967ac7a276e19d86a616a184770aec
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-29 16:04:30 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import '../styles/App.css';
 const App = () => {
+  const[numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
   
-
   return (
     <div id="main">
 
-      <div>
+      <div id="number-display">
+        {numbers.map((numbers) => {
+          return <div key={number}>{nuber}</div>
+        })}
 
+        <button 
+          id="back-button" disabled={numbers[0] < 6 ? true : false} 
+            onClick ={() => {const prevNumbers = numbers.map((number) => { 
+              return number -5
+            }); setNumbers(prevNumbers)
+          }}
+        >
+          Prev Page
+        </button>
+        
+        <button 
+          id="next-button" 
+          onClick ={() => {
+            const nextNumbers = numbers.map((number) => { 
+              return number + 5
+            }); setNumbers(nextNumbers)
+          }}
+        > 
+        Next Page
+        </button>
       </div>
     </div>
   )
